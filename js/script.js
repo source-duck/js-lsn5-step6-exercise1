9f9a4b07c4700ea187907d6755a24f6a3834d9f5
source-duck <sourceduck@duck.com>
2024-12-28 21:19:38 -0500

    Added mouseover events

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var fans = document.querySelector(".fans");
+
+fans.addEventListener("mouseover", function () {
+    fans.innerText = "123k";
+    fans.style.color = "#ffccff";
+});
+
+var fish = document.querySelector(".fish");
+
+fish.addEventListener("mouseover", function () {
+    fish.innerText = "47K";
+    fish.style.color = "#3399ff";
+});
+
+var pets = document.querySelector(".pets");
+
+pets.addEventListener("mouseover", function () {
+    pets.innerText = "20K";
+    pets.style.color = "#33cc33";
+});
