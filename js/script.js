bdffa058ec6b99f8e729fcd128ab0ac9455d6767
source-duck <sourceduck@duck.com>
2024-12-31 21:51:46 -0500

    Updated fans class, mouseover color per testing, better visibility

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
- var themeSwitchInput = document.querySelector("#toggle");
- var ball = document.querySelector(".ball");
- var h1 = document.querySelector("h1");
- var body = document.querySelector("body");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123k";
-    fans.style.color = "#ffccff";
+    fans.style.color = "#ff00ff";
 });
 
 fish.addEventListener("mouseover", function () {
